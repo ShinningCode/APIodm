f4011392f53220ac4208d70f154d5cd9c448ebb5
angelcepeda <jangelcepeda@protonmail.com>
2023-05-15 21:26:20 -0700

    routes created, files for conrollers created

---
+++ routes/proyectRecords.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const router = express.Router();
+const controller = require('../controllers/proyectRecords');
+
+router.get('/', controller.list);
+
+router.get('/:id', controller.index);
+
+router.post('/',controller.create);
+
+router.put('/:id',controller.replace);
+
+router.patch('/:id',controller.update);
+
+router.delete('/:id',controller.destroy);
+
+module.exports = router;
